d02677ca83922fe4cd694f6b1e57ffe48d96c48a
Chris Lam <chris@chrislam.nl>
2021-07-04 17:33:02 +0200

    add binary search

---
+++ algos/binary_search/binary_search.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+int binarySearch(int *arr, int len, int q);
+void printNumbers(int *arr, int len);
+
+int binarySearch(int *arr, int len, int q) {
+  int l = 0;
+  int r = len - 1;
+  while (r - l > 0) {
+    int m = floor((l + r) / 2);
+    if (q < arr[m]) {
+      r = m - 1;
+    }
+    else if (q > arr[m]) {
+      l = m + 1;
+    }
+    else {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+void printNumbers(int *arr, int len) {
+  for (int i=0; i < len; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int len = sizeof(arr) / sizeof(int);
+  int q = 1;
+  printNumbers(arr, len);
+  int inArr = binarySearch(arr, len, q);
+  printf("%d is in arr: %d \n", q, inArr);
+}
+
---
+++ algos/binary_search/binary_search.py
@@ -0,0 +1,22 @@
+from math import floor
+
+def binary_search(li, q):
+  l = 0
+  r = len(li) - 1
+  while r - l > 0:
+    m = floor((r + l) / 2)
+    if q < li[m]:
+      r = m - 1
+    elif q > li[m]:
+      l = m + 1
+    else:
+      return True
+  return False
+
+if __name__ == "__main__":
+  li = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  q = 10
+  in_li = binary_search(li, q)
+  print(li)
+  print(f"{q} is in list: {in_li}")
+
