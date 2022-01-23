09b05c40145777a4607f22d415045dc415f82e64
Chris Lam <chris@chrislam.nl>
2022-01-23 21:29:55 +0100

    type signature

--- algos/bubble_sort/bubblesort.hs
+++ algos/bubble_sort/bubblesort.hs
@@ -9,6 +9,7 @@ bSort [] = []
 bSort a = [(head $ swapped)] ++ (bSort $ tail (swapped))
   where swapped = swap a
 
+main :: IO()
 main = do
   let a = [5, 2, 3, 6, 1, 4, 7, 8, 4, 0, 9]
   print $ bSort a
