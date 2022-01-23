acf8877d98c092950cdd327c6515b3db7374967a
Chris Lam <chris@chrislam.nl>
2022-01-23 21:27:28 +0100

    better bubblesort

--- algos/bubble_sort/bubblesort.hs
+++ algos/bubble_sort/bubblesort.hs
@@ -1,12 +1,13 @@
+swap :: [Int] -> [Int]
+swap [x] = [x]
+swap (x:y:xs) = 
+  if x < y then (swap $ x:xs) ++ [y]
+  else (swap $ y:xs) ++ [x]
 
 bSort :: [Int] -> [Int]
 bSort [] = []
-bSort a = [(head $ swap a)] ++ (bSort $ tail (swap a))
-  where
-    swap [x] = [x]
-    swap (x:y:xs) = 
-      if x < y then (swap $ x:xs) ++ [y]
-      else (swap $ y:xs) ++ [x]
+bSort a = [(head $ swapped)] ++ (bSort $ tail (swapped))
+  where swapped = swap a
 
 main = do
   let a = [5, 2, 3, 6, 1, 4, 7, 8, 4, 0, 9]
