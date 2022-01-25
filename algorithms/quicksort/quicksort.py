6da94f5e20fefca23b4b1b94196f0339e21542c4
Chris Lam <chris@chrislam.nl>
2022-01-25 13:22:40 +0100

    fix issue with duplicate numbers

--- algorithms/quicksort/quicksort.py
+++ algorithms/quicksort/quicksort.py
@@ -1,7 +1,7 @@
 def partition(a, lo, hi):
   p = lo
   for i in range(lo+1, hi):
-    if a[i] < a[p]:
+    if a[i] <= a[p]:
       t = a[i]
       a[i] = a[p]
       a[p] = t
@@ -16,7 +16,7 @@ def quicksort(a, lo, hi):
   quicksort(a, p+1, hi)
 
 if __name__ == "__main__":
-  a = [3, 2, 1, 4, 5, 8, 7, 6]
+  a = [8, 3, 2, 1, 4, 5, 8, 7, 6, 7]
   quicksort(a, 0, len(a))
   print(a)
 
