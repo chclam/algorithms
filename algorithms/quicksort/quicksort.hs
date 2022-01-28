b997ae790387b6c2612ca602d492ffb722a9935d
Chris Lam <chris@chrislam.nl>
2022-01-28 11:23:03 +0100

    correct quicksort

--- algorithms/quicksort/quicksort.hs
+++ algorithms/quicksort/quicksort.hs
@@ -1,16 +1,21 @@
-pivot :: Int -> [Int] -> [Int]
-pivot p [] = [p]
-pivot p (y:ys) = 
-  if p > y then y:(pivot p ys)
-  else (pivot p ys) ++ [y]
+partitionLower :: Int -> [Int] -> [Int]
+partitionLower _ [] = []
+partitionLower p (x:xs) =
+  if x > p then (partitionLower p xs)
+  else x:(partitionLower p xs)
+
+partitionUpper :: Int -> [Int] -> [Int]
+partitionUpper _ [] = []
+partitionUpper p (x:xs) =
+  if x > p then x:(partitionUpper p xs)
+  else (partitionUpper p xs)
 
 quickSort :: [Int] -> [Int]
 quickSort [] = []
 quickSort [x] = [x]
-quickSort (x:xs) = pivot x (quickSort xs)
+quickSort (x:xs) = quickSort(partitionLower x xs) ++ [x] ++ quickSort(partitionUpper x xs)
 
 main :: IO()
 main = do
-  let a = [3, 2, 1, 4, 5, 8, 7, 6]
+  let a = [3, 2, 1, 4, 5, 8, 7, 6, 10, 3]
   print $ quickSort a
-
