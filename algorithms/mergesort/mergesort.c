e127d1e92ae849ba6992dd748311c43b00260640
Chris Lam <chris@chrislam.nl>
2022-04-07 12:23:59 +0200

    redo mergesort in c

--- algorithms/mergesort/mergesort.c
+++ algorithms/mergesort/mergesort.c
@@ -2,31 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-void mergeSort(int *arr, int len) {
+void mergeSort(int *a, int len) {
   if (len == 1) {
     return;
   }
-  else if (len == 2) {
-    if (arr[0] > arr[1]) {
-      int t = arr[0];
-      arr[0] = arr[1];
-      arr[1] = t;
-      return;
+  if (len == 2) {
+    if (a[0] > a[1]) {
+      int t = a[0];
+      a[0] = a[1];
+      a[1] = t;
     }
+    return;
   }
 
-  int m = len / 2;
-  int *l = arr;
-  int *r = arr + m;
-
+  int m = len/2;
+  int *l = a;
+  int *r = a + m;
+  
   mergeSort(l, m);
   mergeSort(r, len - m);
-  
-  int *t = malloc(sizeof(int) * len);
+
+  int *t = malloc(sizeof(int)*len);
   int *rt = t;
-  while(l < arr + m || r < arr + len) {
-    if (l < arr + m && r < arr + len) {
-      if (*l <= *r) {
+  
+  while (1) {
+    if (l < (a+m) && r < (a+len)){
+      if (*l < *r) {
         *t = *l;
         l++;
       }
@@ -35,33 +36,29 @@ void mergeSort(int *arr, int len) {
         r++;
       }
     }
-    else if (l < arr + m) {
+    else if (l < (a+m)) {
       *t = *l;
       l++;
     }
-    else if (r < arr + len) {
+    else if (r < (a+len)) {
       *t = *r;
       r++;
     }
+    else {
+      break;
+    }
     t++;
   }
-  memcpy(arr, rt, sizeof(int) * len);
+  memcpy(a, rt, sizeof(int)*len);
   free(rt);
 }
 
-void printNumbers(int *arr, int len) {
-  for (int i=0; i < len; i++) {
-    printf("%d ", arr[i]);
+int main() {
+  int a[] = {5, 2, 1, 8, 6, 10, 9, 3, 2, 7, 4};
+  int len = sizeof(a)/sizeof(int);
+  mergeSort(a, len);
+  for (int i=0; i<len; i++) {
+    printf("%d ", a[i]);
   }
   printf("\n");
 }
-
-int main() {
-  int arr[10] = {6, 2, 3, 7, 1, 8, 5, 10, 2, 9};
-  int len = sizeof(arr) / sizeof(int);
-  printNumbers(arr, len);
-  mergeSort(arr, len);
-  printNumbers(arr, len);
-  return 0;
-}
-
